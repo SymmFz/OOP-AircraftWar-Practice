308d1b36b09e711862190e5f7dd2a3fe58fc096e
symmfz <symmfz@outlook.com>
2025-09-26 23:56:23 +0800

    refactor: heroAircraft use simple singleton

--- src/edu/hitsz/application/Game.java
+++ src/edu/hitsz/application/Game.java
@@ -74,10 +74,7 @@ public class Game extends JPanel {
     private boolean gameOverFlag = false;
 
     public Game() {
-        heroAircraft = HeroAircraft.getInstance(
-                Main.WINDOW_WIDTH / 2,
-                Main.WINDOW_HEIGHT - ImageManager.HERO_IMAGE.getHeight() ,
-                0, 0, 100);
+        heroAircraft = HeroAircraft.getInstance();
         enemyAircrafts = new LinkedList<>();
         heroBullets = new LinkedList<>();
         enemyBullets = new LinkedList<>();

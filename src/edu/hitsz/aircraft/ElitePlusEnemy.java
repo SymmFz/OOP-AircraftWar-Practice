8984027edefabaa6ced6eb0312ed67f5e0121820
symmfz <symmfz@outlook.com>
2025-10-11 15:30:31 +0800

    refactor: use shoot strategy

--- src/edu/hitsz/aircraft/ElitePlusEnemy.java
+++ src/edu/hitsz/aircraft/ElitePlusEnemy.java
@@ -2,14 +2,17 @@ package edu.hitsz.aircraft;
 
 import edu.hitsz.application.Main;
 import edu.hitsz.bullet.BaseBullet;
-import edu.hitsz.bullet.EnemyBullet;
 import edu.hitsz.item.*;
+import edu.hitsz.shootstrategy.EnemyAircraftScatterShootStrategy;
+import edu.hitsz.shootstrategy.ShootContext;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class ElitePlusEnemy extends EnemyAircraft {
+
+    private ShootContext shootContext = new ShootContext(new EnemyAircraftScatterShootStrategy());
 
     private int shootNum = 3;
     private int power = 5;
@@ -44,21 +47,7 @@ public class ElitePlusEnemy extends EnemyAircraft {
 
     @Override
     public List<BaseBullet> shoot() {
-        List<BaseBullet> res = new LinkedList<>();
-        int x = this.getLocationX();
-        int y = this.getLocationY() + direction * 2;
-        int speedX;
-        int speedY = this.getSpeedY() + direction * 3;
-        BaseBullet bullet;
-        for (int i = 0; i < shootNum; i++) {
-            // 子弹发射位置相对飞机位置向前偏移
-            // 多个子弹横向分散
-            double bulletAngle = (i - (shootNum) / 2) * (double) BULLET_SEP_ANGEL;
-            speedX = (int) (speedY * Math.tan(Math.toRadians(bulletAngle)));
-            bullet = new EnemyBullet(x + (i * 2 - shootNum + 1) * 10, y, speedX, speedY, power);
-            res.add(bullet);
-        }
-        return res;
+        return this.shootContext.shoot(this, this.direction, this.shootNum, this.power);
     }
 
     @Override
